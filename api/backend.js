1621723755340c4e6d5d173f8c5dce4664c01427
Koneh-hub <momohkortu@gmail.com>
2026-08-07 01:11:18 +0000

    node version

--- api/backend.js
+++ api/backend.js
@@ -2,7 +2,7 @@ import serverless from 'serverless-http';
 import app from '../backend/src/app.js';
 
 export const config = {
-  runtime: 'nodejs18.x',
+  runtime: 'nodejs',
 };
 
 export default serverless(app);
